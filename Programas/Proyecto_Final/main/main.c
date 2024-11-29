5772648a423016c77aec60d8f372937a7f5287ef
Eric Piña <51099642+RiQ4@users.noreply.github.com>
2024-11-28 23:47:08 -0400

    Update main.c
    Ya cayó la noche hay que dormir

--- Programas/Proyecto_Final/main/main.c
+++ Programas/Proyecto_Final/main/main.c
@@ -2,8 +2,10 @@
 
 #include <stdlib.h>
 #include <esp_system.h>
+#include <esp_log.h>
 
-#define LED 13
+//#define LED 13
+//#define PB 
 
 #define OFF 0       //ESTADO APAGADO
 #define MEDIUM 1    //ESTADO PARPADEO 0.5s
@@ -11,11 +13,77 @@
 #define SLOW 3      //ESTADO PARPADEO 1s
 #define VARIABLE 4  //ESTADO PARPADEO VARIABLE
 
-bool pb = false;
+struct IO_DATA
+{
+    bool pb;
+    bool led;
+} io_data;
 
-/*-------- FSM --------*/
-void fsm(void) {
 
+uint8_t prev_state = OFF;
+uint8_t curr_state = OFF;
+//uint8_t next_state = 0;
+
+/*-------- FSM FUNCTIONS --------*/
+
+int offFunc(void) {
+    prev_state = curr_state;
+    curr_state = OFF;
+
+    while (1) {
+        if (io_data.pb == true) {
+            io_data.pb = false;
+            return MEDIUM;
+        }
+    }
+}
+
+int mediumFunc(void) {
+    prev_state = curr_state;
+    curr_state = MEDIUM;
+
+    while (1) {
+        if (io_data.pb == true) {
+            io_data.pb = false;
+            return FAST;
+        }
+    }
+}
+
+int fastFunc(void) {
+    prev_state = curr_state;
+    curr_state = FAST;
+
+    while (1) {
+        if (io_data.pb == true) {
+            io_data.pb = false;
+            return SLOW;
+        }
+    }
+}
+
+int slowFunc(void) {
+    prev_state = curr_state;
+    curr_state = SLOW;
+
+    while (1) {
+        if (io_data.pb == true) {
+            io_data.pb = false;
+            return VARIABLE;
+        }
+    }
+}
+
+int varFunc(void) {
+    prev_state = curr_state;
+    curr_state = VARIABLE;
+
+    while (1) {
+        if (io_data.pb == true) {
+            io_data.pb = false;
+            return OFF;
+        }
+    }
 }
 
 void app_main(void)
