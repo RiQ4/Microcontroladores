55f48cb6d9042eadb9ab68bf99c870eece48edc3
Eric Piña <51099642+RiQ4@users.noreply.github.com>
2024-12-02 16:42:03 -0400

    Implementación de las Tareas
    Implementación de las Tareas. Aún no es funcional.

--- Programas/Proyecto_Final/main/main.c
+++ Programas/Proyecto_Final/main/main.c
@@ -19,8 +19,9 @@
 #include "lwip/err.h" //light weight ip packets error handling
 #include "lwip/sys.h" //system applications for light weight ip apps
 
+#include <esp_task_wdt.h>
+
 // #define LED 13
-// #define PB
 
 #define OFF 0      // ESTADO APAGADO
 #define MEDIUM 1   // ESTADO PARPADEO 0.5s
@@ -30,18 +31,21 @@
 
 #define SSID "CLAROA084"
 #define PASS "20212617"
-#define MAX_RETRY 50    //REINTENTOS MAX.
+#define MAX_RETRY 50 // REINTENTOS MAX.
 static EventGroupHandle_t s_wifi_event_group;
 #define WIFI_CONNECTED_BIT BIT0
-#define WIFI_FAIL_BIT      BIT1
+#define WIFI_FAIL_BIT BIT1
 
 static const char *TAG = "Proyecto Final";
 static const char *topic = "micro/esp32s3/pf";
 
-static int retry_num = 0;  //REINTENTOS WIFI
+static int retry_num = 0;    // REINTENTOS WIFI
 bool wifi_connected = false; // ESTADO WIFI
 bool mqtt_connected = false; // ESTADO MQTT
 
+static portMUX_TYPE key = portMUX_INITIALIZER_UNLOCKED;
+
+/*-------- FSM DATA --------*/
 struct IO_DATA
 {
     bool pb;
@@ -51,13 +55,12 @@ struct IO_DATA
 
 uint8_t prev_state = OFF;
 uint8_t curr_state = OFF;
-// uint8_t next_state = 0;
+uint8_t next_state = OFF;
 
 /*-------- FSM FUNCTIONS --------*/
-
 int offFunc(void)
 {
-    prev_state = curr_state;
+    prev_state = OFF;
     curr_state = OFF;
 
     while (1)
@@ -67,6 +70,8 @@ int offFunc(void)
             io_data.pb = false;
             return MEDIUM;
         }
+        else
+            return curr_state;
     }
 }
 
@@ -82,6 +87,8 @@ int mediumFunc(void)
             io_data.pb = false;
             return FAST;
         }
+        else
+            return curr_state;
     }
 }
 
@@ -97,6 +104,8 @@ int fastFunc(void)
             io_data.pb = false;
             return SLOW;
         }
+        else
+            return curr_state;
     }
 }
 
@@ -112,6 +121,8 @@ int slowFunc(void)
             io_data.pb = false;
             return VARIABLE;
         }
+        else
+            return curr_state;
     }
 }
 
@@ -127,6 +138,8 @@ int varFunc(void)
             io_data.pb = false;
             return OFF;
         }
+        else
+            return curr_state;
     }
 }
 
@@ -146,7 +159,7 @@ static void mqtt5_event_handler(void *handler_args, esp_event_base_t base, int32
         ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
         mqtt_connected = true;
         // Subscripcion
-        //msg_id = esp_mqtt_client_subscribe(client, topic, 1);
+        msg_id = esp_mqtt_client_subscribe(client, topic, 2);
 
         break;
     case MQTT_EVENT_DISCONNECTED:
@@ -156,7 +169,7 @@ static void mqtt5_event_handler(void *handler_args, esp_event_base_t base, int32
     case MQTT_EVENT_SUBSCRIBED:
         ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
         ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
-        //msg_id = esp_mqtt_client_publish(client, topic, "entro eri", sizeof("entro eri"), 1, 1);
+        // msg_id = esp_mqtt_client_publish(client, topic, "entro eri", sizeof("entro eri"), 0, 0);
         break;
     case MQTT_EVENT_UNSUBSCRIBED:
         ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
@@ -169,15 +182,26 @@ static void mqtt5_event_handler(void *handler_args, esp_event_base_t base, int32
     case MQTT_EVENT_DATA:
         ESP_LOGI(TAG, "MQTT_EVENT_DATA");
 
+        if (strcmp(event->topic, topic) == 0)
+        {
+            ESP_LOGW(TAG, "TOPIC CORRECTO");
+            printf(event->data[0]);
+            if (strcmp(event->data[0], "1") == 0)
+            {
+                ESP_LOGI(TAG, "Proximo Estado");
+                io_data.pb = true;
+            }
+        }
+
         break;
     case MQTT_EVENT_ERROR:
         ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
         ESP_LOGI(TAG, "MQTT5 return code is %d", event->error_handle->connect_return_code);
         if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT)
         {
-            //log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
-            //log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
-            //log_error_if_nonzero("captured as transport's socket errno", event->error_handle->esp_transport_sock_errno);
+            // log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
+            // log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
+            // log_error_if_nonzero("captured as transport's socket errno", event->error_handle->esp_transport_sock_errno);
             ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
         }
         break;
@@ -190,19 +214,27 @@ static void mqtt5_event_handler(void *handler_args, esp_event_base_t base, int32
 /*-------- WIFI EVENT HANDLER --------*/
 static void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
 {
-    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
+    {
         esp_wifi_connect();
-    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-        if (retry_num < MAX_RETRY) {
+    }
+    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
+    {
+        if (retry_num < MAX_RETRY)
+        {
             esp_wifi_connect();
             retry_num++;
             ESP_LOGI(TAG, "retry to connect to the AP");
-        } else {
+        }
+        else
+        {
             xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
         }
-        ESP_LOGI(TAG,"connect to the AP fail");
-    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "connect to the AP fail");
+    }
+    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
+    {
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
         ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
         retry_num = 0;
         xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
@@ -212,29 +244,29 @@ static void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_b
 /*-------- WIFI INIT CONFIG --------*/
 void wifi_init(void)
 {
-    s_wifi_event_group = xEventGroupCreate();           //Evento FreeRTOS para WiFi
+    s_wifi_event_group = xEventGroupCreate(); // Evento FreeRTOS para WiFi
 
-    //ESP_ERROR_CHECK(esp_netif_init());                          //Inicializacion Network Interface
-    //ESP_ERROR_CHECK(esp_event_loop_create_default());           //No se
-    esp_netif_create_default_wifi_sta();                        //Config Network Interface para STATION
+    // ESP_ERROR_CHECK(esp_netif_init());                          //Inicializacion Network Interface
+    // ESP_ERROR_CHECK(esp_event_loop_create_default());           //No se
+    esp_netif_create_default_wifi_sta(); // Config Network Interface para STATION
 
-    wifi_init_config_t wifi_init = WIFI_INIT_CONFIG_DEFAULT();  // Config INIT por defecto
-    ESP_ERROR_CHECK(esp_wifi_init(&wifi_init));                 //WiFi INIT
+    wifi_init_config_t wifi_init = WIFI_INIT_CONFIG_DEFAULT(); // Config INIT por defecto
+    ESP_ERROR_CHECK(esp_wifi_init(&wifi_init));                // WiFi INIT
 
-    esp_event_handler_instance_t instance_any_id;               //Handler de Evento Cualquiera
-    esp_event_handler_instance_t instance_got_ip;               //Handler Evento de IP
+    esp_event_handler_instance_t instance_any_id; // Handler de Evento Cualquiera
+    esp_event_handler_instance_t instance_got_ip; // Handler Evento de IP
 
     ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
                                                         ESP_EVENT_ANY_ID,
                                                         &wifi_event_handler,
                                                         NULL,
-                                                        &instance_any_id));     //Registro Evento wifi
-    
+                                                        &instance_any_id)); // Registro Evento wifi
+
     ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
                                                         IP_EVENT_STA_GOT_IP,
                                                         &wifi_event_handler,
                                                         NULL,
-                                                        &instance_got_ip));     //Registro Evento IP                                                  
+                                                        &instance_got_ip)); // Registro Evento IP
 
     wifi_config_t wifi_config = {
         .sta = {
@@ -251,25 +283,27 @@ void wifi_init(void)
 
     /* Esto lo saqué del ejemplo. Solo entendí que es para verificar si se logró la conexión */
     EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
-            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
-            pdFALSE,
-            pdFALSE,
-            portMAX_DELAY);
+                                           WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+                                           pdFALSE,
+                                           pdFALSE,
+                                           portMAX_DELAY);
 
-
-    if (bits & WIFI_CONNECTED_BIT) {
+    if (bits & WIFI_CONNECTED_BIT)
+    {
         ESP_LOGI(TAG, "connected to ap SSID:%s", SSID);
         wifi_connected = true;
-
-    } else if (bits & WIFI_FAIL_BIT) {
+    }
+    else if (bits & WIFI_FAIL_BIT)
+    {
         ESP_LOGI(TAG, "Failed to connect to SSID:%s", SSID);
-    } else {
+    }
+    else
+    {
         ESP_LOGE(TAG, "UNEXPECTED EVENT");
     }
 }
 
 /*-------- MQTT CLIENT CONFIG --------*/
-
 esp_mqtt_client_handle_t client = NULL;
 static void mqtt5_app_start(void)
 {
@@ -286,6 +320,70 @@ static void mqtt5_app_start(void)
     esp_mqtt_client_start(client);
 }
 
+/*-------- TASKS FUNCTIONS --------*/
+void input(void)
+{
+    while (1)
+    {
+        if (next_state == OFF)
+        {
+            next_state = offFunc();
+        }
+        else if (next_state == MEDIUM)
+        {
+            next_state = mediumFunc();
+        }
+        else if (next_state == FAST)
+        {
+            next_state = fastFunc();
+        }
+        else if (next_state == SLOW)
+        {
+            next_state = slowFunc();
+        }
+        else if (next_state == VARIABLE)
+        {
+            next_state = varFunc();
+        }
+    }
+    vTaskDelay(10/portTICK_PERIOD_MS);
+}
+
+void output(void)
+{
+    char buffer[50];
+    while (1)
+    {
+        if (next_state != curr_state)
+        {
+            if (curr_state == OFF)
+            {
+                sprintf(buffer, "Cambiando al estado: %s\n", "OFF");
+            }
+            else if (curr_state == SLOW)
+            {
+                sprintf(buffer, "Cambiando al estado: %s\n", "SLOW");
+            }
+            else if (curr_state == MEDIUM)
+            {
+                sprintf(buffer, "Cambiando al estado: %s\n", "MEDIUM");
+            }
+            else if (curr_state == FAST)
+            {
+                sprintf(buffer, "Cambiando al estado: %s\n", "FAST");
+            }
+            else if (curr_state == VARIABLE)
+            {
+                sprintf(buffer, "Cambiando al estado: %s\n", "VARIABLE");
+            }
+            taskENTER_CRITICAL(&key);
+            printf(buffer);
+            taskEXIT_CRITICAL(&key);
+        }
+        vTaskDelay(10/portTICK_PERIOD_MS);
+    }
+}
+
 void app_main(void)
 {
     esp_err_t ret = nvs_flash_init();
@@ -297,13 +395,23 @@ void app_main(void)
     ESP_ERROR_CHECK(ret);
 
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-    ESP_ERROR_CHECK(esp_netif_init()); 
+    ESP_ERROR_CHECK(esp_netif_init());
 
     wifi_init();
 
+    // chequear esto
     while (mqtt_connected != true)
     {
         mqtt5_app_start();
         vTaskDelay(5000 / portTICK_PERIOD_MS); // Delay para darle chance
     }
+
+    esp_task_wdt_config_t config = {
+        .timeout_ms = 60000,
+        .trigger_panic = true,
+        .idle_core_mask = 0, // i.e. do not watch any idle task
+    };
+    esp_err_t err = esp_task_wdt_reconfigure(&config);
+    xTaskCreate(input, "Entrada", 1024, NULL, 1, NULL);
+    xTaskCreate(output, "Salida", 1024, NULL, 1, NULL);
 }
